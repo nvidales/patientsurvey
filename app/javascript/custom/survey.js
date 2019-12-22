67f576fbcee5724b1f7d4078f6ab46e8abb21337
nvidales <nancyvidales@gmail.com>
2019-12-21 20:41:10 -0600

    first commit

---
+++ app/javascript/custom/survey.js
@@ -0,0 +1,26 @@
+questions = ["1 of 5: Courtesy of staff who admitted you",
+              "2 of 5: Room cleanliness",
+            "3 of 5:  Noise level in and around your room",
+            "4 of 5: Quality of food",
+            "5 of 5: Friendliness/courtesy of nurses"];
+
+questionsAnswered = 1;
+$(".question h2").html(questions[0]);
+
+$(".answers div").click(function(){
+  //alert("clicked");
+
+  if(questionsAnswered < questions.length){
+    console.log(questions[questionsAnswered]);
+
+    $(".question h2").html(questions[questionsAnswered]);
+    questionsAnswered += 1;
+  } else {
+    //alert("survey finished");
+    $(".question").toggle();
+    $(".answers").toggle();
+    $(".patient-info").css("display", "grid");
+  }
+
+
+})
